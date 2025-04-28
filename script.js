1dce670eab459ff1b5c6706272648c056b79ec03
matti96 <116589476+matti96@users.noreply.github.com>
2025-04-28 17:31:02 +0200

    Add toggleInfoVisibility
    Add eventListener for each tile to toggle visibility of info section on click

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+
+let tiles = document.querySelectorAll(".project-tile");
+
+tiles.forEach(function(tile) {
+    let info = tile.querySelector(".info-container");
+
+    info.style.display = "none";
+    tile.addEventListener('click', function(){
+        toggleInfoVisibility(this);
+    })
+})
+
+
+
+
+function toggleInfoVisibility(obj) {
+    let info = obj.querySelector(".info-container");
+
+    if(info.style.display == "none") {
+        info.style.display = "inline";
+    } else {
+        info.style.display = "none";
+    }
+}
