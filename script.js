6bd5dd09fa7b26297539cb5b6bf0783c1d790a0d
matti96 <116589476+matti96@users.noreply.github.com>
2025-04-29 15:30:33 +0200

    Change logic of hiding grid tiles
    All tiles turn invisible before clicked tile turns visible

--- script.js
+++ script.js
@@ -11,12 +11,18 @@ tiles.forEach(function(tile) {
 })
 
 
-
+function turnAllInvisible(objList) {
+    objList.forEach(function(obj) {
+        let info = obj.querySelector(".info-container");
+        info.style.display = "none";
+    })
+}
 
 function toggleInfoVisibility(obj) {
     let info = obj.querySelector(".info-container");
 
     if(info.style.display == "none") {
+        turnAllInvisible(tiles);
         info.style.display = "inline";
     } else {
         info.style.display = "none";
