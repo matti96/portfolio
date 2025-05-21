940aa982bfbee55325e4f243d0efaa8a77b7716c
matti96 <116589476+matti96@users.noreply.github.com>
2025-05-21 15:12:00 +0200

    Restructure project
    Use tiles and dedicated display area instead of expanding tiles

--- script.js
+++ script.js
@@ -1,49 +1,26 @@
-const tileCollapse = "200px";
-const tileExpand = "400px";
+const tileHandler = (function(){
 
-let tiles = document.querySelectorAll(".project-tile");
+    const tiles = document.querySelectorAll(".project-tile");
+    tiles.forEach(function(tile, index){
+        tile.addEventListener('click', function(){
+            displayHandler.show(index + ": " + tile);
+        });
+    });
 
-tiles.forEach(function(tile) {
-    let info = tile.querySelector(".info-container");
-
-    info.style.display = "none";
-    tile.addEventListener('click', function(){
-        toggleInfoVisibility(this);
-    })
-})
-
-
-let closeButtons = document.querySelectorAll(".close-info");
-closeButtons.forEach(function(btn) {
-    btn.addEventListener('click', function(btn){
-        btn.stopPropagation();
-        turnAllInvisible(tiles);
-    })
-})
-
-
-function turnAllInvisible(objList) {
-    console.log("wtf")
-    objList.forEach(function(obj) {
-        let info = obj.querySelector(".info-container");
-        info.style.display = "none";
-        obj.style.width = tileCollapse;
-    })
-}
-
-function turnVisible(obj) {
-    let info = obj.querySelector(".info-container");
-    info.style.display = "inline";
-    obj.style.width = tileExpand;
-}
-
-function toggleInfoVisibility(obj) {
-    let info = obj.querySelector(".info-container");
-
-    if(info.style.display == "none") {
-        turnAllInvisible(tiles);
-        turnVisible(obj);
-    } else {
-        //turnAllInvisible(tiles);
+    function saySomething() {
+        console.log("something");
+        return 1;
     }
-}
+
+    return { saySomething };
+})();
+
+const displayHandler = (function(){
+
+    const display = document.querySelector("#display");
+    function show(content){
+        display.innerText = "" + content;
+    }
+
+    return { show };
+})();
